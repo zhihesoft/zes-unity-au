14a1ff4874177ad21e9cf732f3a1d41a5ebda524
stanley <houyuqing@hotmail.com>
2022-09-11 23:30:07 +0800

    add di container

---
+++ Packages/zes.game.au/Runtime/Scripts/DI/Container.cs
@@ -0,0 +1,17 @@
+namespace Au.DI
+{
+    public class Container
+    {
+        public Container(Container parent)
+        {
+            this.parent = parent;
+        }
+
+        public readonly Container parent;
+
+        public Container CreateChild()
+        {
+            return new Container(this);
+        }
+    }
+}
